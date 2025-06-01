accb8378cc4f614090a812411b3f22b1f92120df
Steven Billich <stevenbillich17@gmail.com>
2025-06-01 18:31:25 +0300

    added basic parser

--- src/main.py
+++ src/main.py
@@ -1,1 +1,4 @@
-print("Something is wrong with the code")
+import qiskit
+from qiskit import QuantumCircuit
+
+QuantumCircuit.from_qasm_file("qasm2/ghz.qasm").draw(output="mpl", scale=0.5, style={"name": "iqx"}).savefig("qasm2/ghz.png")
